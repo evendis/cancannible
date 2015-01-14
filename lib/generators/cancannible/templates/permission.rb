5132dde2ccd1fb10147499b7594ca01eb5697894
Paul Gallagher <gallagher.paul@gmail.com>
2015-01-14 22:21:21 +0800

    add attr_accessible note to generated model

--- lib/generators/cancannible/templates/permission.rb
+++ lib/generators/cancannible/templates/permission.rb
@@ -3,6 +3,11 @@ class Permission < ActiveRecord::Base
   belongs_to :permissible, polymorphic: true
   belongs_to :resource, polymorphic: true
 
+  # Note: for Rails 3 you may need to declare attr_accessible as follows, depending on your whitelist_attributes setting.
+  # A future version of cancannible should make this unnecessary.
+  #
+  # attr_accessible :asserted, :ability, :resource_id, :resource_type
+
   validates_uniqueness_of :ability, scope: [:resource_id, :resource_type, :permissible_id, :permissible_type]
 
 end
