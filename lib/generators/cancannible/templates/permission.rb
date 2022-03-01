e11098fb5fcf39b0b44de1349998540938bf0f2a
Paul Gallagher <gallagher.paul@gmail.com>
2022-03-01 11:33:02 +0800

    permission#resource is optional

--- lib/generators/cancannible/templates/permission.rb
+++ lib/generators/cancannible/templates/permission.rb
@@ -1,13 +1,12 @@
 # The Permission class stores permissions managed by CanCan and Cancannible
 class Permission < ActiveRecord::Base
   belongs_to :permissible, polymorphic: true
-  belongs_to :resource, polymorphic: true
+  belongs_to :resource, polymorphic: true, optional: true
+
+  validates :ability, uniqueness: { scope: [:resource_id, :resource_type, :permissible_id, :permissible_type] }
 
   # Note: for Rails 3 you may need to declare attr_accessible as follows, depending on your whitelist_attributes setting.
   # A future version of cancannible should make this unnecessary.
   #
   # attr_accessible :asserted, :ability, :resource_id, :resource_type
-
-  validates_uniqueness_of :ability, scope: [:resource_id, :resource_type, :permissible_id, :permissible_type]
-
 end
--- spec/support/models.rb
+++ spec/support/models.rb
@@ -1,13 +1,10 @@
 # These model definitions are just used for the test scenarios
 
-# The Permission class stores permissions maanged by CanCan and Cancannible
 class Permission < ActiveRecord::Base
   belongs_to :permissible, polymorphic: true
-  belongs_to :resource, polymorphic: true
+  belongs_to :resource, polymorphic: true, optional: true
 
-  validates_uniqueness_of :ability,
-    :scope => [:resource_id, :resource_type,
-      :permissible_id, :permissible_type]
+  validates :ability, uniqueness: { scope: [:resource_id, :resource_type, :permissible_id, :permissible_type] }
 end
 
 class Member < ActiveRecord::Base
